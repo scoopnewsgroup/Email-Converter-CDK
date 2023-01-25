6f2607f88b8d362eaf44b76c5185e53591fac31a
Spenser Wellen <spenser.wellen@scoopnewsgroup.com>
2023-01-25 10:54:37 -0800

    DEVS-391 Updated implementation for lib file Lambda function deployment

--- lib/email-converter-cdk-stack.ts
+++ lib/email-converter-cdk-stack.ts
@@ -1,18 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
-// import { S3EventSource } from 'aws-cdk/bin/c';
 import { Construct } from 'constructs';
-// import {
-// 	NodejsFunction,
-// 	NodejsFunctionProps,
-// } from 'aws-cdk-lib/aws-lambda-nodejs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EmailConverterCdkStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
 		// Lambda Functions
-		// Here is another implementation that I've seen
 		const convertEmailLambda = new cdk.aws_lambda.Function(
 			this,
 			'ConvertEmailHandler',
@@ -28,34 +21,15 @@ export class EmailConverterCdkStack extends cdk.Stack {
 			this,
 			'MyEmailConversionBucket'
 		);
-		emailConversionBucket.addEventNotification(
-			cdk.aws_s3.EventType.OBJECT_CREATED,
+
+		const emailConversionEventSource =
 			new cdk.aws_lambda_event_sources.S3EventSource(
 				emailConversionBucket,
 				{
 					events: [cdk.aws_s3.EventType.OBJECT_CREATED],
 				}
-			).bind(convertEmailLambda)
-		);
+			);
 
-		// Here was an initial idea I had for implemnetation from what I've seen on our internal stack...
-		// const convertEmail = new NodejsFunction(
-		// 	this,
-		// 	'convertEmailFunction',
-		// 	{
-		// 		entry: 'src/lambda/targets/convertEmailTarget.ts',
-		// 		...NodejsFunctionProps,
-		// 		environment: {
-		// 			...NodejsFunctionProps.environment,
-		// 		},
-		// 	}
-		// );
-
-		// The code that defines your stack goes here
-
-		// example resource
-		// const queue = new sqs.Queue(this, 'EmailConverterCdkQueue', {
-		//   visibilityTimeout: cdk.Duration.seconds(300)
-		// });
+		convertEmailLambda.addEventSource(emailConversionEventSource);
 	}
 }
